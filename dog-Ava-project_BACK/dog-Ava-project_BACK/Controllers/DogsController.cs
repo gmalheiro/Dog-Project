515e9c188f9ec18028c60ce93feba845348e94fe
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-17 16:57:41 -0300

    Adding files to github

---
+++ dog-Ava-project_BACK/dog-Ava-project_BACK/Controllers/DogsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dog_Ava_project_BACK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogsController : ControllerBase
+    {
+    }
+}
